59956657046c1b63e315ea90765637c22e9d0dba
Ivan Lee <ivanklee86@gmail.com>
2024-08-18 15:16:11 +0000

    Start working on filtering Projects by labels, focusing on submaps.

---
+++ pkg/cli/filter_test.go
@@ -0,0 +1,33 @@
+package cli
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMapSubset(t *testing.T) {
+	bigMap := make(map[string]string)
+	bigMap["a"] = "x"
+	bigMap["b"] = "y"
+	bigMap["c"] = "z"
+
+	matchingMap := make(map[string]string)
+	matchingMap["a"] = "x"
+	matchingMap["b"] = "y"
+
+	notMatchingMap := make(map[string]string)
+	notMatchingMap["d"] = "u"
+
+	differentValueMap := make(map[string]string)
+	differentValueMap["a"] = "c"
+
+	t.Run("Map contains a submap", func(t *testing.T) {
+		assert.Equal(t, true, IsMapSubset(bigMap, matchingMap))
+	})
+
+	t.Run("Map does not contains a submap", func(t *testing.T) {
+		assert.Equal(t, false, IsMapSubset(bigMap, notMatchingMap))
+		assert.Equal(t, false, IsMapSubset(bigMap, differentValueMap))
+	})
+}
