9f24e25c7b1fadb0401a97a5b54e34919e60da74
Ivan Lee <ivanklee86@gmail.com>
2024-08-19 06:04:29 -0400

    Filter projects (#14)
    * Start working on filtering Projects by labels, focusing on submaps.
    
    * Make isMapSubset private.
    
    * Add tests for project filtering.

---
+++ pkg/cli/filter.go
@@ -0,0 +1,29 @@
+package cli
+
+import (
+	"github.com/argoproj/argo-cd/v2/pkg/apis/application/v1alpha1"
+)
+
+func isMapSubset[K, V comparable](m, sub map[K]V) bool {
+	if len(sub) > len(m) {
+		return false
+	}
+	for k, vsub := range sub {
+		if vm, found := m[k]; !found || vm != vsub {
+			return false
+		}
+	}
+	return true
+}
+
+func filterProjects(appProjects *v1alpha1.AppProjectList, labels map[string]string) []v1alpha1.AppProject {
+	matchingProjects := []v1alpha1.AppProject{}
+
+	for _, appProject := range appProjects.Items {
+		if isMapSubset(appProject.ObjectMeta.Labels, labels) {
+			matchingProjects = append(matchingProjects, appProject)
+		}
+	}
+
+	return matchingProjects
+}
--- pkg/cli/octanap.go
+++ pkg/cli/octanap.go
@@ -1,24 +1,33 @@
 package cli
 
 import (
+	"context"
 	"fmt"
 	"io"
 	"os"
+	"time"
 
 	"github.com/ivanklee86/octanap/pkg/client"
 )
 
+const TIMEOUT = 120
+
 type Config struct {
-	ServerAddr string
-	Insecure   bool
-	AuthToken  string
+	ServerAddr      string
+	Insecure        bool
+	AuthToken       string
+	DryRun          bool
+	Filters         map[string]string
+	SyncWindowsFile string
 }
 
 // Octanap is the logic/orchestrator.
 type octanap struct {
 	*Config
 
-	ArgoCDClient client.IArgoCDClient
+	// Client
+	ArgoCDClient          client.IArgoCDClient
+	ArgoCDClientConnected bool
 
 	// Allow swapping out stdout/stderr for testing.
 	Out io.Writer
@@ -56,4 +65,16 @@ func (o *octanap) Connect() {
 	}
 
 	o.ArgoCDClient = argocdClient
+	o.ArgoCDClientConnected = true
+}
+
+func (o *octanap) SetSyncWindows() {
+	ctxTimeout, cancel := context.WithTimeout(context.Background(), time.Second*TIMEOUT)
+	defer cancel()
+
+	_, err := o.ArgoCDClient.ListProjects(ctxTimeout)
+	if err != nil {
+		o.Error(fmt.Sprintf("Error fetching Projects: %s", err.Error()))
+	}
+
 }
