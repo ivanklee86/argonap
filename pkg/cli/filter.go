bf983b8ca347940f86c400fc1ac44c7660cddbcc
Ivan Lee <ivanklee86@gmail.com>
2024-08-19 19:23:57 -0400

    Filter on syncwindows (#15)
    * Filter on existance SyncWindows (so we can clear them).
    
    * Filter on existance SyncWindows (so we can clear them).
    
    * Fix linting.

--- pkg/cli/filter.go
+++ pkg/cli/filter.go
@@ -16,12 +16,19 @@ func isMapSubset[K, V comparable](m, sub map[K]V) bool {
 	return true
 }
 
-func filterProjects(appProjects *v1alpha1.AppProjectList, labels map[string]string) []v1alpha1.AppProject {
+func filterProjects(appProjects *v1alpha1.AppProjectList, labels map[string]string, hasSyncWindow bool) []v1alpha1.AppProject {
 	matchingProjects := []v1alpha1.AppProject{}
 
 	for _, appProject := range appProjects.Items {
 		if isMapSubset(appProject.ObjectMeta.Labels, labels) {
-			matchingProjects = append(matchingProjects, appProject)
+			// Additional filter to only selct projects with SyncWindows.
+			if hasSyncWindow {
+				if len(appProject.Spec.SyncWindows) > 0 {
+					matchingProjects = append(matchingProjects, appProject)
+				}
+			} else {
+				matchingProjects = append(matchingProjects, appProject)
+			}
 		}
 	}
 
